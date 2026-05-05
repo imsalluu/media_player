37c3df193458ace8a06bac6d131e5b61ea700452
imsalluu <flutterdev.salman@gmail.com>
2026-05-05 16:51:21 +0600

    update the manifest file

--- android/app/src/main/kotlin/com/example/media_player/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/media_player/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.media_player
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
